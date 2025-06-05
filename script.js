d91664aa3a3bf2937e391439ef0682469606944f
jaoofc123 <105529532+jaoofc123@users.noreply.github.com>
2025-06-05 14:15:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -16,14 +16,38 @@ const SESSION_KEY = "xfut_session";
 
 // Lista de jogos e opções (agora com data e hora exata de início)
 const jogos = [
-  { 
-  nome: "Equador x Brasileira(M) (05.06)",
-  inicio: "2025-06-14T21:00:00-03:00",
+    { 
+  nome: "Espanha(M) x França(M) (05.06)",
+  inicio: "2025-06-05T16:00:00-03:00",
   opcoes: [
-    { nome: "Opção 1", url: "https://jaoofc123.github.io/xfut/" },
-    { nome: "Opção 2", url: "https://jaoofc123.github.io/xfut/" },
-    { nome: "Opção 3", url: "https://jaoofc123.github.io/xfut/" },
-    { nome: "Opção 4", url: "https://jaoofc123.github.io/xfut/" }
+    { nome: "Opção 1", url: "https://nossoplayeronlinehd.lat/tv/espn" },
+    { nome: "Opção 2", url: "https://nossoplayeronlinehd.lat/tv/sportv" },
+    { nome: "Opção 3", url: "https://daddylive.dad/futemax/stream-88.php" },
+    { nome: "Opção 4", url: "https://embedcanaistv.com/sportv/" },
+    { nome: "Opção 5", url: "https://reidoscanais.cc/embed/?id=sportv" }
+  ]
+},
+      { 
+  nome: "Botafogo SP x Coritiba (05.06)",
+  inicio: "2025-06-05T18:00:00-03:00",
+  opcoes: [
+    { nome: "Opção 1", url: "https://nossoplayeronlinehd.net/tv/espn" },
+    { nome: "Opção 2", url: "https://nossoplayeronlinehd.lat/extra/eventos3" },
+    { nome: "Opção 3", url: "https://daddylive.mp/futemax/stream-81.php" },
+    { nome: "Opção 4", url: "https://embedcanaistv.com/espn/" },
+    { nome: "Opção 5", url: "https://reidoscanais.cc/embed/?id=espn" }
+  ]
+},
+  { 
+  nome: "Equador(M) x Brasil(M) (05.06)",
+  inicio: "2025-06-05T20:00:00-03:00",
+  opcoes: [
+    { nome: "Opção 1", url: "https://nossoplayeronlinehd.lat/tv/globosp" },
+    { nome: "Opção 2", url: "https://nossoplayeronlinehd.lat/tv/sportv" },
+    { nome: "Opção 3", url: "https://nossoplayeronlinehd.lat/extra/eventos1" },
+    { nome: "Opção 4", url: "https://daddylive.dad/futemax/stream-78.php" },
+    { nome: "Opção 5", url: "https://reidoscanais.cc/embed/?id=sportv" },
+    { nome: "Opção 6", url: "https://reidoscanais.vip/embed/?id=globosp-globosaopaulo" }
   ]
 },
 { 
