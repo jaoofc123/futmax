bd25d20e0d94b5fd930445bd21e20aaedb2b5013
jaoofc123 <105529532+jaoofc123@users.noreply.github.com>
2025-06-17 11:17:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -17,73 +17,47 @@ const SESSION_KEY = "xfut_session";
 // Lista de jogos e opções (agora com data e hora exata de início)
 const jogos = [
 {
-  nome: "Chelsea x León",
-  inicio: "2025-06-16T16:00:00-03:00",
-  opcoes: [
-    { nome: "Opção 1", url: "https://nossoplayeronlinehd.live/tv/sportv" },
-    { nome: "Opção 2", url: "https://nossoplayeronlinehd.live/tv/caze1" },
-    { nome: "Opção 3", url: "https://reidoscanais.vip/embed/?id=sportv" },
-    { nome: "Opção 4", url: "https://embedcanaistv.com/sportv/" }
-  ]
-},
-{
-  nome: "Boca Juniors x Benfica",
-  inicio: "2025-06-16T19:00:00-03:00",
-  opcoes: [
-    { nome: "Opção 1", url: "https://nossoplayeronlinehd.live/tv/sportv" },
-    { nome: "Opção 2", url: "https://nossoplayeronlinehd.live/tv/caze1" },
-    { nome: "Opção 3", url: "https://reidoscanais.vip/embed/?id=sportv" },
-    { nome: "Opção 4", url: "https://embedcanaistv.com/sportv/" }
-  ]
-},
-{
-  nome: "Flamengo x Espérance",
-  inicio: "2025-06-16T22:00:00-03:00",
-  opcoes: [
-    { nome: "Opção 1", url: "https://nossoplayeronlinehd.live/tv/sportv" },
-    { nome: "Opção 2", url: "https://nossoplayeronlinehd.live/tv/caze1" },
-    { nome: "Opção 3", url: "https://reidoscanais.vip/embed/?id=sportv" },
-    { nome: "Opção 4", url: "https://embedcanaistv.com/sportv/" }
-  ]
-},
-{
   nome: "Fluminense x Borussia",
   inicio: "2025-06-17T13:00:00-03:00",
   opcoes: [
-    { nome: "Opção 1", url: "" },
-    { nome: "Opção 2", url: "" },
-    { nome: "Opção 3", url: "" },
-    { nome: "Opção 4", url: "" }
+    { nome: "Opção 1", url: "https://nossoplayeronlinehd.live/tv/sportv" },
+    { nome: "Opção 2", url: "https://nossoplayeronlinehd.live/tv/caze1" },
+    { nome: "Opção 3", url: "https://reidoscanais.vip/embed/?id=sportv" },
+    { nome: "Opção 4", url: "https://embedcanaistv.com/sportv/" },
+    { nome: "Opção 5", url: "https://www.youtube.com/@CazeTV" }
   ]
 },
 {
   nome: "River Plate x Urawa Reds",
   inicio: "2025-06-17T16:00:00-03:00",
   opcoes: [
-    { nome: "Opção 1", url: "" },
-    { nome: "Opção 2", url: "" },
-    { nome: "Opção 3", url: "" },
-    { nome: "Opção 4", url: "" }
+    { nome: "Opção 1", url: "https://nossoplayeronlinehd.live/tv/sportv" },
+    { nome: "Opção 2", url: "https://nossoplayeronlinehd.live/tv/caze1" },
+    { nome: "Opção 3", url: "https://reidoscanais.vip/embed/?id=sportv" },
+    { nome: "Opção 4", url: "https://embedcanaistv.com/sportv/" },
+    { nome: "Opção 5", url: "https://www.youtube.com/@CazeTV" }
   ]
 },
 {
   nome: "Ulsan Hyundai x Sundowns",
   inicio: "2025-06-17T19:00:00-03:00",
   opcoes: [
-    { nome: "Opção 1", url: "" },
-    { nome: "Opção 2", url: "" },
-    { nome: "Opção 3", url: "" },
-    { nome: "Opção 4", url: "" }
+    { nome: "Opção 1", url: "https://nossoplayeronlinehd.live/tv/sportv" },
+    { nome: "Opção 2", url: "https://nossoplayeronlinehd.live/tv/caze1" },
+    { nome: "Opção 3", url: "https://reidoscanais.vip/embed/?id=sportv" },
+    { nome: "Opção 4", url: "https://embedcanaistv.com/sportv/" },
+    { nome: "Opção 5", url: "https://www.youtube.com/@CazeTV" }
   ]
 },
 {
   nome: "Monterrey x Inter",
   inicio: "2025-06-17T22:00:00-03:00",
   opcoes: [
-    { nome: "Opção 1", url: "" },
-    { nome: "Opção 2", url: "" },
-    { nome: "Opção 3", url: "" },
-    { nome: "Opção 4", url: "" }
+    { nome: "Opção 1", url: "https://nossoplayeronlinehd.live/tv/sportv" },
+    { nome: "Opção 2", url: "https://nossoplayeronlinehd.live/tv/caze1" },
+    { nome: "Opção 3", url: "https://reidoscanais.vip/embed/?id=sportv" },
+    { nome: "Opção 4", url: "https://embedcanaistv.com/sportv/" },
+    { nome: "Opção 5", url: "https://www.youtube.com/@CazeTV" }
   ]
 },
 {
